553341947418000b6caa43c55379cc1b87b6f0af
rohitprajapati-37-boomlive <179814179+rk-prajapati-37@users.noreply.github.com>
2025-11-16 21:35:12 +0530

    Add experience schema to both schemas and schemaTypes indexes

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import project from './project'
+import blog from './blog'
+import skill from './skill'
+import experience from './experience'
+import testimonial from './testimonial'
+import contact from './contact'
+
+export const schemaTypes = [project, blog, skill, experience, testimonial, contact]
--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,8 @@
 import project from './project'
 import blog from "./blog";
 import skill from './skill'
+import experience from './experience'
 import testimonial from './testimonial'
 import contact from './contact'
 
-export const schemaTypes = [project, blog, skill, testimonial, contact]
+export const schemaTypes = [project, blog, skill, experience, testimonial, contact]
